24e99937efedaa623d5acb9da0d3f9d1e20c64cf
j13939 <dan.reynebeau@flexagon.com>
2024-09-24 07:12:37 -0500

    "testing out script"

--- commands.sh
+++ commands.sh
@@ -8,7 +8,7 @@ setOutput()
         echo "Key and value required on setOutput function call"
         exit 1
     fi
-    echo "${2}" > "/u01/flexdeploy/application/localhost/work/91798633/9412378/internal/outputs/${1}"
+    echo "${2}" > "/u01/flexdeploy/application/localhost/work/91798633/9423469/internal/outputs/${1}"
 }
 
 
